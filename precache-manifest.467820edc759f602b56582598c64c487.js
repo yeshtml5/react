287bf3bd60b51867ef30f626feaa9f7609dd5e26
wanhwi.son <yeshtml5@gmail.com>
2020-04-11 09:11:39 +0900

    Updates

---
+++ precache-manifest.467820edc759f602b56582598c64c487.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7edf95f4664014432ea9687facac53ff",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "cbf2b6f4473a7977f344",
+    "url": "/react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fa9d297e7d9fa4a72c4a",
+    "url": "/react/static/js/2.0de8d930.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.0de8d930.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbf2b6f4473a7977f344",
+    "url": "/react/static/js/main.e7587044.chunk.js"
+  },
+  {
+    "revision": "2faaa1eea16373ed087a",
+    "url": "/react/static/js/runtime-main.65e7200c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
