74381d5ae15150015eb92af10357ab9f0e8ec62c
wanhwi.son <yeshtml5@gmail.com>
2020-04-11 10:10:17 +0900

    text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         새로운희망
+         도전
         </p>
         <a
           className="App-link"
