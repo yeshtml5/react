c47988185aef292cdaa852ff83017bafa322e6bc
wanhwi.son <yeshtml5@gmail.com>
2020-04-19 11:30:44 +0900

    스와이퍼 업데이트

--- src/components/ui/swiper.js
+++ src/components/ui/swiper.js
@@ -1,21 +1,54 @@
+/**
+ * @title 스와이퍼
+ * @example https://react-id-swiper.ashernguyen.site/example/default
+ */
 import React from 'react'
 import Swiper from 'react-id-swiper'
+import styled from 'styled-components'
+//style
+import 'swiper/css/swiper.css' // Version >= 2.4.0
 
 export default () => {
-  const params = {
+  let params = {
+    lazy: true,
+    shouldSwiperUpdate: true, //업데이트될때마다 re-render
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
       dynamicBullets: true
+    },
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      hide: false
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
     }
   }
+  //---------------------------------------------------------------------
+
+  //---------------------------------------------------------------------
   return (
     <Swiper {...params}>
-      <div>Slide #1</div>
-      <div>Slide #2</div>
-      <div>Slide #3</div>
-      <div>Slide #4</div>
-      <div>Slide #5</div>
+      <div>
+        <img src="https://source.unsplash.com/random" />
+      </div>
+      <div>
+        <img src="https://source.unsplash.com/random" />
+      </div>
+      <div>
+        <img src="https://source.unsplash.com/random" />
+      </div>
+      <div>
+        <img src="https://source.unsplash.com/random" />
+      </div>
+      <div>
+        <img src="https://source.unsplash.com/random" />
+      </div>
+      <div>
+        <img src="https://source.unsplash.com/random" />
+      </div>
     </Swiper>
   )
 }
